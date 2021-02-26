705f5ebfc9a435b10aa039d88347c9c52041ec2b
TranVietAn <an.tran@nfq.asia>
2021-02-26 17:28:10 +0700

    first commit

---
+++ Assets/Libraries/UICreator/Scripts/UIController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private List<UIPanel> _UIPanels;
+
+    // Store UI panel following opening order
+    private Stack<UIPanel> _panelStack = new Stack<UIPanel>();
+
+    public void PushToStack(UIPanel panel)
+    {
+        if (_panelStack.Count > 0)
+        {
+            var recentPanel = _panelStack.Peek();
+            recentPanel.SetInteractable(!recentPanel.IsOpening);
+        }
+
+        _panelStack.Push(panel);
+    }
+
+    public void PopFromStack()
+    {
+        if (_panelStack.Count == 0)
+        {
+            return;
+        }
+
+        _panelStack.Pop();
+
+        if (_panelStack.Count == 0)
+        {
+            return;
+        }
+
+        var recentPanel = _panelStack.Peek();
+        if (recentPanel.IsOpening)
+        {
+            recentPanel.SetInteractable(true);
+        }
+        else
+        {
+            PopFromStack();
+        }
+    }
+
+    private void Start()
+    {
+        InitAllUIPanels();
+    }
+
+    private void InitAllUIPanels()
+    {
+        for (int i = 0; i < _UIPanels.Count; i++)
+        {
+            var panel = _UIPanels[i];
+            if (panel == null)
+            {
+                // Log
+                continue;
+            }
+
+            panel.Init(this);
+
+            if (panel.ShowFromStart)
+            {
+                panel.Open();
+            }
+        }
+    }
+}
